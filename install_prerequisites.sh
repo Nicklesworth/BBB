2387e660315a3be36a9088f79c1d77a5eff06d97
Chris Nicolls <christopher.nicolls@gmail.com>
2025-02-24 11:58:21 -0500

    Add prerequisite script

---
+++ install_prerequisites.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Run this script once to install the necessary prerequisites for building the bootloader
+# This script is intended to be run on a fresh Ubuntu 24.04 installation
+sudo apt-get install -y bison flex libssl-dev libgnutls28-dev gcc-arm-linux-gnueabihf
